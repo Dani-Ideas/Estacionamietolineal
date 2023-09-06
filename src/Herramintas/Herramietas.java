2d41d8c460c995cf776e8efce6353cc260277dfb
Dani-Ideas <daniideas0@gamil.com>
2023-09-06 11:29:42 -0600

    rellenado el array con numetos randoms

--- src/Herramintas/Herramietas.java
+++ src/Herramintas/Herramietas.java
@@ -1,5 +1,14 @@
 package Herramintas;
+import java.util.Random;
 
 public class Herramietas {
-	esta es la rama estacionamieto
+
+	public static void main(String[] args) {
+		int estacionamieto[]= new int[100];
+		Random numero_ramdomRandom=new Random();
+		for(int i =0; i<estacionamieto.length-1; i++) {
+			estacionamieto[i]=numero_ramdomRandom.nextInt(100);
+		}
+		
+}
 }
