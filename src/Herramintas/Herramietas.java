177d1e5ade0d38dc44c6dad4754704d3f73de70d
Dani-Ideas <daniideas0@gamil.com>
2023-09-06 18:51:34 -0600

    se agrego la sentecia de desicion para determminar que carros entran y que carros salen del estacionamito, solo falta traer los metodos de array que manipulen el array de la ramma master

--- src/Herramintas/Herramietas.java
+++ src/Herramintas/Herramietas.java
@@ -6,9 +6,19 @@ public class Herramietas {
 	public static void main(String[] args) {
 		int estacionamieto[]= new int[100];
 		Random numero_ramdomRandom=new Random();
+		// para rellenar el array
 		for(int i =0; i<estacionamieto.length-1; i++) {
-			estacionamieto[i]=numero_ramdomRandom.nextInt(100);
+			estacionamieto[i]=i+1;
+			System.out.println(estacionamieto[i]);
 		}
-		
+		// es para decidir que numero de carro en la posicion del array se retiran de carros del estacionamieto
+		for (int i= 0; i<estacionamieto.length-1; i++) {
+			int desicion=numero_ramdomRandom.nextInt(100);
+			if (desicion<=49) {
+				// si el numero entra dentro de este rango de ejcutara el inetercabio de las posiciones 
+			}else {
+				//si no etra detro del rango del numero generado 
+			}
+		}
 }
 }
