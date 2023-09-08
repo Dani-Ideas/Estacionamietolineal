f9feac6a327091a9b745d130134dccb66e12dbc7
Dani-Ideas <daniideas0@gamil.com>
2023-09-07 23:30:59 -0600

    cambiando el nombre algunas variables usando cammmel case

--- src/Herramintas/Herramietas.java
+++ src/Herramintas/Herramietas.java
@@ -6,7 +6,7 @@ public class Herramietas {
 
 	public static void main(String[] args) { try {
 		Arreglos <Integer> estacionamieto=new Arreglos<Integer>(20);
-		Random numero_ramdomRandom=new Random();
+		Random numberRandom=new Random();
 		
 		// para rellenar el array
 		for(int i =0; i<=estacionamieto.longitud()-1; i++) {
@@ -16,10 +16,10 @@ public class Herramietas {
 		
 		// es para decidir el carro dentro del estacionamieto es retirado o se queda
 		for (int i= 0; i<=estacionamieto.longitud()-1; i++) {
-			int desition=numero_ramdomRandom.nextInt(2);
+			int choose=numberRandom.nextInt(2);
 			// si el numero entra dentro de este rango del no se ejecutara el inetercabio de las posiciones 
-			if (desition==1) {
-				/* Este rango fue selecionado por que el metodo nextInt(100) genera numeros aleatorios del 0 al 100 este metodo puede ser modificado en proximas versiones
+			if (choose==1) {
+				/* Este rango fue selecionado por que el metodo nextInt(2) genera numeros aleatorios del 0 al 1 este metodo puede ser modificado en proximas versiones
 				 *  de la aberracion llamada Java
 				 * */
 				System.out.println("-----------------------------------\n El carrro "+i+" permanece dentro del estacionamiento\n");
@@ -29,15 +29,21 @@ public class Herramietas {
 				estacionamieto.asignar(i, -1);
 				estacionamieto.imprime();
 				int calle=estacionamieto.ultimo();
-				// en  este caso el -2 se va atomar como null en lo que encutro una manera de que calle pueda alamacenar null
+				// en  este caso el -2 se va a tomar como null en lo que encuetro una manera de que calle pueda alamacenar null
+				// en  este caso el -2 se va a tomar como null en lo que encuetro una manera de que calle pueda alamacenar null
+				// en  este caso el -2 se va a tomar como null en lo que encuetro una manera de que calle pueda alamacenar null
+				// en  este caso el -2 se va a tomar como null en lo que encuetro una manera de que calle pueda alamacenar null
+				// en  este caso el -2 se va a tomar como null en lo que encuetro una manera de que calle pueda alamacenar null
 				while(calle!=-1&&estacionamieto.ultimo()!=-1) {
 					for (int j= estacionamieto.longitud()-2;j>=0;j--) {
 						estacionamieto.asignar(j+1, estacionamieto.recupera(j));
 						if (j==0) {
 							estacionamieto.asignar(0, calle);
 							calle=estacionamieto.ultimo();
-							if (calle==-1)
+							if (calle==-1) {
+								estacionamieto.asignar(estacionamieto.longitud()-1, null/*-2*/);
 								System.out.println(" ________ ___  ________      \n"+ "|\\  _____\\\\  \\|\\   ___  \\    \n"+ "\\ \\  \\__/\\ \\  \\ \\  \\\\ \\  \\   \n"+ " \\ \\   __\\\\ \\  \\ \\  \\\\ \\  \\  \n"+ "  \\ \\  \\_| \\ \\  \\ \\  \\\\ \\  \\ \n"+ "   \\ \\__\\   \\ \\__\\ \\__\\\\ \\__\\\n"+ "    \\|__|    \\|__|\\|__| \\|__|");
+							}
 						}
 						estacionamieto.imprime();
 						System.out.println("Otra iteracion para sacar el carro\n\n\n\n");
